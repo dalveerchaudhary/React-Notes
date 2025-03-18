97bcc5b75af3f81433cb65d141925e9bf4fd3272
dalveersingh <dalveerchu1@gmail.com>
2025-03-18 22:02:03 +0530

    new project create e-commerce

---
+++ Second Part/project/src/components/Footer.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { FaTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+
+const Footer = () => {
+	return (
+		<footer id='footer'
+			className="bg-gray-800 text-white py-4 mt-20">
+			<div className="container mx-auto grid 
+	grid-cols-4 gap-4 text-center">
+				<div className="flex flex-col justify-center">
+					<h2 className="text-lg font-bold">
+						Let's Create
+					</h2>
+					<input type="email" placeholder="Enter your email"
+						className="mt-2 px-4 py-2 bg-gray-700 text-white 
+		border border-gray-600 rounded-md focus:ring
+			focus:border-green-400" />
+				</div>
+
+				<div className="flex flex-col justify-center">
+					<h2 className="text-lg font-semibold">
+						Our Services
+					</h2>
+					<ul className="mt-2">
+						<li className="hover:text-gray-300">
+							Inspiration Gallery
+						</li>
+						<li className="hover:text-gray-300">
+							Case Studies
+						</li>
+						<li className="hover:text-gray-300">
+							Trend Insights
+						</li>
+						<li className="hover:text-gray-300">
+							Workshops and Webinars
+						</li>
+						<li className="hover:text-gray-300">
+							Community Involvement
+						</li>
+					</ul>
+				</div>
+
+				<div className="flex flex-col justify-center">
+					<h2 className="text-lg font-bold">
+						Our Company
+					</h2>
+					<ul className="mt-2">
+						<li className="hover:text-gray-300">
+							Our Services
+						</li>
+						<li className="hover:text-gray-300">
+							Home Section
+						</li>
+					</ul>
+				</div>
+
+				<div className="flex flex-col justify-center">
+					<h2 className="text-lg font-bold">Contact Us</h2>
+					<address className="mt-2">
+						Don Bosco Street, Ottawa, Canada
+					</address>
+					<p className="mt-1">Phone: +1234567890</p>
+					<p>Email: letscreate153@gmail.com</p>
+					<div className="mt-2 flex justify-center">
+						<a href="#" className="mr-2 text-4xl 
+			hover:text-green-300 cursor-default">
+							<FaTwitter />
+						</a>
+						<a href="#" className="mr-2 text-4xl 
+			hover:text-green-300 cursor-default">
+							<FaInstagramSquare />
+						</a>
+						<a href="#" className="hover:text-green-300
+			text-4xl cursor-default">
+							<FaFacebook />
+						</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ Second Part/project/src/components/Header.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div id='header'
+			className='flex items-center
+	justify-between'>
+			<div className='text-[24px] 
+	ml-[80px] font-bold underline'>
+				Let's Create
+			</div>
+			<div>
+				<ul className='flex cursor-pointer m-6'>
+					<a href="#aboutUS">
+						<li
+							className='p-6 text-[20px] 
+			hover:text-blue-950 
+			text-orange-800 
+			transition-all delay-75
+				hover:underline hover:delay-75
+			'>
+							<a href="#header">About Us</a>
+						</li>
+					</a>
+					<li
+						className='p-6 text-[20px] 
+			hover:text-blue-950 text-orange-800
+			transition-all delay-75
+			hover:underline hover:delay-75
+			'>
+						<a href="#services">Services</a>
+					</li>
+					<li
+						className='p-6 text-[20px]
+			hover:text-blue-950 
+			text-orange-800 
+			transition-all delay-75
+			hover:underline hover:delay-75 
+			'>
+						<a href="#contact">Contact Us</a>
+					</li>
+					<li
+						className='p-6 text-[20px] 
+			hover:text-blue-950 
+			text-orange-800 
+			transition-all delay-75
+			hover:underline hover:delay-75
+			'>
+						<a href="#footer">Footer</a>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<button className=' border-green-800 
+		text text-2xl 
+		text-green-800
+		mr-[35px] 
+		border-4 p-[6px]
+		rounded-md hover:bg-green-800
+		hover:text-white
+			hover:transition-all
+			delay-75'>Let's Talk</button>
+			</div>
+		</div>
+	)
+}
+
+export default Header;
